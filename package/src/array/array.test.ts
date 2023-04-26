e127ffad9d44867f5a6abca0911555499868d99a
Andrey Potyomkin <and_tem@icloud.com>
2023-04-26 15:03:37 +0300

    test(UIKIT-676): Добавлен тест кейс для array

--- package/src/array/array.test.ts
+++ package/src/array/array.test.ts
@@ -1,4 +1,10 @@
-import { ValidationArrayError, createSimpleError } from '../core';
+import {
+  ValidationArrayError,
+  createArrayError,
+  createErrorMap,
+  createSimpleError,
+} from '../core';
+import { object } from '../object';
 
 import { array } from './array';
 import { ARRAY_TYPE_ERROR_INFO } from './constants';
@@ -12,24 +18,6 @@ describe('array', () => {
       expect(error?.cause.code).toBe(ARRAY_TYPE_ERROR_INFO.code);
     },
   );
-
-  it('Возвращает массив ошибок', () => {
-    const errorCode = Symbol();
-
-    const error = array((value, ctx) =>
-      ctx.createError({ message: String(value), code: errorCode }),
-    )([1, 2, 3]);
-
-    const expectedErrors = [
-      createSimpleError({ message: '1', code: errorCode }),
-      createSimpleError({ message: '2', code: errorCode }),
-      createSimpleError({ message: '3', code: errorCode }),
-    ];
-
-    expect((error as ValidationArrayError).cause.errorArray).toEqual(
-      expectedErrors,
-    );
-  });
 
   it('Каждый элемент массива ошибок соответствует результату выполнения правила валидации для item', () => {
     const errorCode = Symbol();
@@ -51,5 +39,37 @@ describe('array', () => {
     expect((error as ValidationArrayError).cause.errorArray).toEqual(
       expectedErrors,
     );
+  });
+
+  it('Возвращает ошибку, содержащую массив ошибок для каждого item', () => {
+    const nameError = createSimpleError({
+      message: 'invalid name',
+      code: Symbol(),
+    });
+    const ageError = createSimpleError({
+      message: 'invalid age',
+      code: Symbol(),
+    });
+
+    const value = [
+      { name: 'name', age: 22 },
+      { name: 'name', age: 22 },
+    ];
+
+    const validateArray = array(
+      object<{ name: string; age?: number }>({
+        name: () => nameError,
+        age: () => ageError,
+      }),
+    );
+
+    const error = validateArray(value);
+
+    const expectedError = createArrayError([
+      createErrorMap({ name: nameError, age: ageError }),
+      createErrorMap({ name: nameError, age: ageError }),
+    ]);
+
+    expect(error).toEqual(expectedError);
   });
 });
