62690b1116574bc723b4bc5c6b7c2360edaed61a
Andrey Potyomkin <and_tem@icloud.com>
2023-05-15 17:10:34 +0300

    chore(UIKIT-750): Настроен lint-staged для resolver

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,12 @@ module.exports = {
     () => 'npm run lint:types --workspace=@astral/revizor',
   ],
 
+  'react-hook-form-resolver/**/*.{js,jsx,ts,tsx}': [
+    'npm run lint --workspace=@astral/revizor-react-hook-form-resolver',
+    () =>
+      'npm run lint:types --workspace=@astral/revizor-react-hook-form-resolver',
+  ],
+
   'pack/**/*.{js}': ['npm run lint --workspace=@astral/pack'],
 
   'PRTitleLinter/**/*.{js}': ['npm run lint --workspace=@astral/PRTitleLinter'],
