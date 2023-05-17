58e44cf3fd7ddb05fb54eaa232a581ec00330dba
Yaroslav <62848436+YrStudenikin@users.noreply.github.com>
2023-05-17 17:33:02 +0300

    feat(UIKIT-738): Реализован email (#25)

---
+++ package/src/email/email.test.ts
@@ -0,0 +1,58 @@
+import { expect } from 'vitest';
+
+import {
+  EMAIL_MAX_LENGTH,
+  INVALID_EMAIL_ERROR_INFO,
+  LENGTH_EMAIL_ERROR_INFO,
+} from './constants';
+import { email } from './email';
+
+const getLongEmail = () => `${'a'.repeat(EMAIL_MAX_LENGTH)}@test.com`;
+
+describe('email', () => {
+  it.each<string>(['a', '@mail.ru', 'mail.ru', 'test@.ru', 'test.ru@'])(
+    'Invalid for: %s',
+    (value) => {
+      const validate = email();
+      const error = validate(value);
+
+      expect(error?.cause.code).toEqual(INVALID_EMAIL_ERROR_INFO.code);
+    },
+  );
+
+  it.each<string>([
+    'test@test.ru',
+    'test@test.com',
+    'test-t@test.ru',
+    'test.t@test.ru',
+    'test_t@test.ru',
+  ])('Valid for: %s', (value) => {
+    const validate = email();
+    const result = validate(value);
+
+    expect(result).toBeUndefined();
+  });
+
+  it('Validate default invalid length message', () => {
+    const validate = email();
+    const error = validate(getLongEmail());
+
+    expect(error?.message).toBe(LENGTH_EMAIL_ERROR_INFO.message);
+  });
+
+  it('Validate custom message', () => {
+    const customMessage = 'CustomMessage';
+    const validate = email({ message: customMessage });
+    const error = validate('test@');
+
+    expect(error?.message).toBe(customMessage);
+  });
+
+  it('Validate custom invalid length message', () => {
+    const customMessage = 'CustomMessage';
+    const validate = email({ invalidLengthMessage: customMessage });
+    const error = validate(getLongEmail());
+
+    expect(error?.message).toBe(customMessage);
+  });
+});
