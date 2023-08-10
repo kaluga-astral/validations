ffdda57dfad298f2578f139fa751c86dad58f6b4
Ilya <p9067700518@gmail.com>
2023-08-10 16:35:28 +0300

    bug(UIKIT-819): Правит сообщения об ошибках КПП

--- package/src/kpp/constants.ts
+++ package/src/kpp/constants.ts
@@ -2,17 +2,17 @@ import { ErrorInfo, createErrorCode } from '../core';
 
 export const KPP_PATTERN_ERROR_INFO: ErrorInfo = {
   code: createErrorCode('kpp-invalid-pattern'),
-  message: 'Проверьте КПП: не соответствует общим правилам формирования',
+  message: 'Проверьте КПП',
 };
 
 export const KPP_ZEROS_ONLY_ERROR_INFO: ErrorInfo = {
   code: createErrorCode('kpp-invalid-zeros-only'),
-  message: 'Проверьте КПП: не может состоять только из нулей',
+  message: 'Не может состоять только из нулей',
 };
 
 export const KPP_DOUBLE_ZERO_START_ERROR_INFO: ErrorInfo = {
   code: createErrorCode('kpp-invalid-double-zero-start'),
-  message: 'Проверьте КПП: не может начинаться с 00',
+  message: 'Не может начинаться с 00',
 };
 
 export const KPP_REGEX = /^(\d{4}([A-Z]|\d){2}\d{3})$/;
