1335445d3d41b3f357b86e10b47bc72b78fb3124
IgorBabikov <igorbabikov91@gmail.com>
2023-08-10 14:45:48 +0300

    feat(UIKIT-785): Реализованы правила валидации для паспортных данных (#60)

---
+++ package/src/passportCode/constants.ts
@@ -0,0 +1,16 @@
+import { ErrorInfo, createErrorCode } from '../core';
+
+export const PASSPORT_CODE_ERROR_INFO: ErrorInfo = {
+  code: createErrorCode('passportCode'),
+  message: 'Проверьте код',
+};
+
+export const PASSPORT_CODE_LENGTH_ERROR_INFO: ErrorInfo = {
+  code: createErrorCode('passportCode'),
+  message: 'Длина поля должна быть равна 6 символам',
+};
+
+export const PASSPORT_CODE_ONLY_DIGITS_ERROR_INFO: ErrorInfo = {
+  code: createErrorCode('passportCode'),
+  message: 'Только цифры',
+};
---
+++ package/src/passportSeries/passportSeries.ts
@@ -0,0 +1,58 @@
+import { CommonRuleParams, createRule, isNoDoubleZeroStart } from '../core';
+import { onlyNumber } from '../onlyNumber';
+
+import {
+  PASSPORT_SERIES_ERROR_INFO,
+  PASSPORT_SERIES_ERROR_LENGTH_INFO,
+  PASSPORT_SERIES_ONLY_DIGITS_ERROR_INFO,
+} from './constants';
+
+type PassportSeriesParams = CommonRuleParams<string> & {
+  /**
+   * @description Замена стандартного сообщения ошибки.
+   */
+  message?: string;
+};
+
+/**
+ * @description Проверяет валидна ли серия паспорта
+ * @example
+ * ```ts
+ * const validate = string(passportSeries());
+ * validate('9217');
+ * ```
+ */
+
+export const passportSeries = <
+  TLastSchemaValues extends Record<string, unknown>,
+>(
+  params?: PassportSeriesParams,
+) =>
+  createRule<string, TLastSchemaValues>(
+    (value, ctx) => {
+      if (onlyNumber()(value)) {
+        return ctx.createError({
+          message:
+            params?.message || PASSPORT_SERIES_ONLY_DIGITS_ERROR_INFO.message,
+          code: PASSPORT_SERIES_ONLY_DIGITS_ERROR_INFO.code,
+        });
+      }
+
+      if (value.length !== 4) {
+        return ctx.createError({
+          message: params?.message || PASSPORT_SERIES_ERROR_LENGTH_INFO.message,
+          code: PASSPORT_SERIES_ERROR_LENGTH_INFO.code,
+        });
+      }
+
+      if (!isNoDoubleZeroStart(value)) {
+        return ctx.createError({
+          message: params?.message || PASSPORT_SERIES_ERROR_INFO.message,
+          code: PASSPORT_SERIES_ERROR_INFO.code,
+        });
+      }
+
+      return undefined;
+    },
+    { exclude: params?.exclude },
+  );
