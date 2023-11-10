a0a6147316f2ce1ce8022307c726fe1b04f20ac3
Bblchock <105416811+Bblchock@users.noreply.github.com>
2023-11-10 09:20:42 +0300

    feat(UIKIT-817): Добавлено правило валидации innFL (#75)
    Добавлено правило валидации innFL и innTwelveSymbols

---
+++ package/src/innFL/constants.ts
@@ -0,0 +1,6 @@
+import { ErrorInfo, createErrorCode } from '../core';
+
+export const INN_FL_ERROR_INFO: ErrorInfo = {
+  code: createErrorCode('innFL'),
+  message: 'Некорректный ИНН ФЛ',
+};
---
+++ package/src/innTwelveSymbols/constants.ts
@@ -0,0 +1,14 @@
+import { ErrorInfo, createErrorCode } from '../core';
+
+export const INN_12_SYMBOLS_ERROR_INFO: ErrorInfo = {
+  code: createErrorCode('innTwelveSymbols'),
+  message: 'Некорректный ИНН',
+};
+
+export const INN_12_SYMBOLS_LENGTH = 12;
+
+export const FIRST_INN_12_SYMBOLS_DECODING = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
+
+export const SECOND_INN_12_SYMBOLS_DECODING = [
+  3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8,
+];
