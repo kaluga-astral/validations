6fdbab4dee94c408e11aa36860e80141d61f437e
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-08-07 10:48:34 +0300

    bug(UIKIT-1686): Исправлено сообщение об ошибки Снилс и ОГРН

--- package/src/ogrnUL/constants.ts
+++ package/src/ogrnUL/constants.ts
@@ -2,7 +2,7 @@ import { type ErrorInfo, createErrorCode } from '../core';
 
 export const OGRN_UL_ERROR_INFO: ErrorInfo = {
   code: createErrorCode('ogrnUL'),
-  message: 'Некорректный ОГРН ЮЛ',
+  message: 'Проверьте ОГРНЮЛ',
 };
 
 export const OGRN_UL_LENGTH = 13;
