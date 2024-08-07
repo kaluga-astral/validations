34ab5c3169d9f476b3ad9352cba63f409c469283
Королев Данил <105650840+pan1caisreal@users.noreply.github.com>
2024-08-07 13:11:57 +0300

    bug(UIKIT-1686): Правки по PR

--- package/src/ogrnUL/constants.ts
+++ package/src/ogrnUL/constants.ts
@@ -2,7 +2,7 @@ import { type ErrorInfo, createErrorCode } from '../core';
 
 export const OGRN_UL_ERROR_INFO: ErrorInfo = {
   code: createErrorCode('ogrnUL'),
-  message: 'Проверьте ОГРНЮЛ',
+  message: 'Проверьте ОГРН',
 };
 
 export const OGRN_UL_LENGTH = 13;
